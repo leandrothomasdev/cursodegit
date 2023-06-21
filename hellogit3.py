87aacfad6d14e6dda6cbc042132d5c8695c499df
Leandro Dev <leandrothomasdev@gmail.com>
2023-06-21 20:48:46 -0300

    cambio

--- hellogit3.py
+++ hellogit3.py
@@ -1,16 +1,16 @@
-print("Hello git master");
-print("Hello git master");
-print("Hello git final");
-print("Hello git master");
-print("Hello git master");
-print("Hello git final");
-print("Hello git master");
-print("Hello git master");
 print("Hello git login");
-print("Hello git master");
-print("Hello git master");
-print("Hello git caca");
-print("Hello git master");
-print("Hello git master");
+print("Hello git login");
+print("Hello git final");
+print("Hello git login");
+print("Hello git login");
+print("Hello git final");
+print("Hello git login");
+print("Hello git login");
+print("Hello git login");
+print("Hello git login");
+print("Hello git login");
+print("Hello git login");
+print("Hello git login");
+print("Hello git login");
 
 
